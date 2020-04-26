c0ed01dace2c0a48f98e61f8939f63cf84f3b4b0
Vladislav Kalugin <kaluginvlad97@gmail.com>
2020-04-26 14:18:43 +0300

    Add liqpay models

---
+++ LiqPay/Models/LiqpayFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LiqPay.Models
+{
+    /// <summary>
+    /// Data that is passed to the form view for generate a payment button Liqpay  
+    /// </summary>
+    public class LiqpayFormViewModel
+    {
+        public string Data { get; set; }
+        public string Signature { get; set; }
+    }
+}
